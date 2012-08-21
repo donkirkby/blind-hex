385e0820be77e76aa87df1b34d8150095aad24bd
Don Kirkby <donkirkby@gmail.com>
2012-08-20 22:16:20 -0700

    Fix centring and remove spines from outer edges. Start working on different tick counts.

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 from math import sin, pi
 
 #echo on
-#echo width 60
+#echo width 40
 #echo scroll 50
 #echo turtle
 
@@ -14,18 +14,30 @@ class GameBoard:
         count = 11
         height = t.window_height()
         width = t.window_width()
-        d1 = height / (count * 1.5)
+        d1 = height*.97 / (count * 1.5)
         d2 = width / count
         d = min(d1, d2)
         t.penup()
         t.right(90)
-        t.fd(height/2 - d)
-        t.left(90)
-        t.back(width/2 - d)
+        t.fd(d * (count-1)/2)
+        t.right(60)
+        t.fd(d * (count-1)/2)
+        t.left(150)
         t.pendown()
-        for _ in range(count/2 + 1):
-            for __ in range(count/2 + 1):
-                self.hexagon(d)
+        for i in range(count/2 + 1):
+            for j in range(count/2 + 1):
+                skip_headings = []
+                if i == 0:
+                    skip_headings.append(180)
+                elif i == count/2:
+                    skip_headings.append(0)
+                if j == 0:
+                    skip_headings.append(300)
+                elif j == count/2:
+                    skip_headings.append(120)
+                if len(skip_headings) == 2:
+                    skip_headings.append(sum(skip_headings)/2)
+                self.hexagon(d, skip_headings)
                 t.penup()
                 t.left(90)
                 t.fd(2*d)
@@ -40,31 +52,53 @@ class GameBoard:
             t.right(30)
             t.pendown()
 
-    def edge(self, t, d):
-        ticksize = d/5
-        t.fd(d / 2)
+
+    def tick(self, t, ticksize):
         t.left(90)
         t.penup()
-        t.fd(ticksize/2)
+        t.fd(ticksize / 2)
         t.pendown()
         t.back(ticksize)
         t.penup()
-        t.fd(ticksize/2)
+        t.fd(ticksize / 2)
         t.pendown()
         t.right(90)
-        t.fd(d / 2)
 
-    def hexagon(self, height):
+    def edge(self, t, d):
+        ticksize = d/8
+        stepcount = 12
+        tickcount = 4
+        for i in range(stepcount):
+            parity_match = i % 2 != tickcount % 2
+            distance_from_centre = abs(stepcount/2 - i)
+            close_enough = distance_from_centre < tickcount 
+            if parity_match and close_enough:
+                self.tick(t, ticksize)
+            t.fd(d / stepcount)
+
+    def hexagon(self, height, skip_headings):
+        """ Draw a hexagon of the given height, centred at the current 
+        position. 
+        """
         d = height / (2*sin(60*pi/180))
         t = self.t
+        t.penup()
+        t.fd(d)
+        t.left(120)
+        t.pendown()
         for _ in range(6):
             self.edge(t, d)
-            t.left(60)
-            t.fd(d)
-            t.penup()
-            t.back(d)
-            t.pendown()
-            t.right(120)
+            t.right(60)
+            if t.heading() not in skip_headings:
+                t.fd(d)
+                t.penup()
+                t.back(d)
+                t.pendown()
+            t.left(120)
+        t.penup()
+        t.right(120)
+        t.back(d)
+        t.pendown()
 
 if __name__ == '__live_coding__':
     global __live_turtle__
@@ -74,5 +108,8 @@ elif __name__ == '__main__':
     from Tkinter import mainloop
     from turtle import Turtle
     
-    GameBoard(Turtle()).draw()
+    t = Turtle()
+    t.tracer(1000000) # disable animation
+    GameBoard(t).draw()
+    t.tracer(1)
     mainloop()
